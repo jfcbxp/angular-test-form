136f6b2a0dd3d060f9fc9e4809d349298abcaf07
Joao Felipe <jfcbxp@gmail.com>
2022-08-26 10:10:07 -0300

    adicionado funcoes todo-list

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+  styleUrls: ['./todo-input-add-itens.component.scss'],
 })
 export class TodoInputAddItensComponent implements OnInit {
+  @Output() public emmitItemTaskList = new EventEmitter();
 
-  constructor() { }
+  public addItemTaskList: string = '';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if (this.addItemTaskList) {
+      this.emmitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = '';
+    }
   }
-
 }
